d28e02789723ecd5c375f1bc430f0a03a13ec4b2
anitharandheep <107831982+anitharandheep@users.noreply.github.com>
2022-06-27 10:11:32 +0530

    Day2

---
+++ Project/Developerfactory/projects/matrimony/matrimonyfrontend/src/Signup.js
@@ -0,0 +1,25 @@
+import './Signup.css'
+import { useNavigate } from "react-router-dom";
+function Signup() {
+    const navigate = useNavigate();
+    const profile = (a) => { navigate("/Profile") };
+
+    return (
+        <div>
+            <div className='box'>
+
+                <div className='row1'><label>Login</label><button>x</button></div>
+                <div className='row2'><label>Matrimony Id/Mobile No./E-mail</label><input type="Text" /></div>
+                <div className='row3'><label>Password</label><input type="Password" /></div>
+                <div className='row4'><input type="checkbox" /><label>"Keep me logged in"</label></div>
+                <div className='row5'><button onClick={(a) => { profile(a); }}>LOGIN</button></div>
+                <div className='row6'>
+                    <div className='buttonleft'><button>Forgot Password?</button></div>
+                    <div className='buttonright'><button>Log in via OPT</button></div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Signup;
